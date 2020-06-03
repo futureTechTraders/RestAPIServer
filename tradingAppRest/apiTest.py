770c05516686ae8c9d8e10222953cf161acbb617
naslevente <levente.nas@gmail.com>
2020-06-03 16:01:01 -0400

    Adding MovingAverages.py and playing with apiTest.py

--- tradingAppRest/apiTest.py
+++ tradingAppRest/apiTest.py
@@ -3,6 +3,6 @@ import urllib.request
 
 #reponse = urllib.request.urlopen(sys.rgv[1])
 #html = response.read()
-#num = 6 + int(sys.argv[1])
-print("First Name: " + sys.argv[1])
+num = 6 + int(sys.argv[1])
+print("First Name: " + str(num))
 sys.stdout.flush()
