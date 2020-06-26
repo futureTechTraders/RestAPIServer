7d529f876aa15cc124a00eef5917f8b96f926635
akhil <akhilmaheeps12@gmail.com>
2020-06-25 20:09:43 -0400

    Added pre&post market hour data capabilites

--- tradingAppRest/DataScrape.py
+++ tradingAppRest/DataScrape.py
@@ -20,16 +20,17 @@ class TickerDataFrame:
         
     def createDataFrame(self):
         
-        symbol = yf.Ticker(self.tickerSymbol)
+        #symbol = yf.Ticker(self.tickerSymbol)
         #print(str(time.strftime("%I:%M:%S"))[0:2])
         
         if (str(time.strftime("%I:%M:%S%p"))[8:] == "PM" and int(str(time.strftime("%I:%M:%S"))[0:2]) >= 4) or (str(time.strftime("%I:%M:%S%p"))[8:] == "AM" and int(str(time.strftime("%I:%M:%S"))[0:2]) <= 12):
-            
-            tickerData = symbol.history(period = self.period)
+            #below two lines changed to provide pre & post market data
+            tickerData = yf.download(tickers = self.tickerSymbol,period = self.period, prepost = True)
+            #tickerData = symbol.history(period = self.period)
         else:
             
-            tickerData = symbol.history(start = self.start)
-        
+            tickerData = yf.download(tickers = self.tickerSymbol,start = self.start, prepost = True)
+            #tickerData = symbol.history(start = self.start)
         #tickerData = symbol.history(start = self.start, end = self.end)
         return tickerData
     
